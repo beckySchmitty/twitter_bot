f81cacbe03830f87118879d5afe2499d9f44360d
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2020-12-20 15:14:13 -0500

    tweepy auth

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 """Twitter bot"""
+import tweepy
+import secret
+
+auth = tweepy.OAuthHandler(secret.CONSUMER_KEY, secret,CONSUMER_SECRET)
+auth.set_access_token(secret.ACCESS_KEY, secret.ACCESS_SECRET)
+api = tweepy.API(auth)
+
+api.update_status("Test tweet from Becky's laptop")
