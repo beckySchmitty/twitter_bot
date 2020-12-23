9c1f64849dae20dfeb1126a7e8ae8f3d1409d786
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2020-12-23 15:30:03 -0500

    need to fix unicode error

--- app.py
+++ app.py
@@ -5,15 +5,13 @@ import secret
 from bs4 import BeautifulSoup
 import requests
 
-auth = tweepy.OAuthHandler(secret.CONSUMER_KEY, secret.CONSUMER_SECRET)
-auth.set_access_token(secret.ACCESS_KEY, secret.ACCESS_SECRET)
-api = tweepy.API(auth)
+# auth = tweepy.OAuthHandler(secret.CONSUMER_KEY, secret.CONSUMER_SECRET)
+# auth.set_access_token(secret.ACCESS_KEY, secret.ACCESS_SECRET)
+# api = tweepy.API(auth)
 
-api.update_status("Another tweet from Becky's laptop")
+# api.update_status("Another tweet from Becky's laptop")
 
-
-
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
-url='https://www.nytimes.com/'
-response=requests.get(url,headers=headers)
-print(response)
+URL = 'https://stackoverflow.com/questions/14630288/unicodeencodeerror-charmap-codec-cant-encode-character-maps-to-undefined'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup)
