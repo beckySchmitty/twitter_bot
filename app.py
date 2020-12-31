cf9220b786b86cb591e3dbd6334b6c2d3df47066
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2020-12-30 19:02:17 -0500

    adding weather api & datetime

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 """Twitter bot"""
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
+app = Flask(__name__)
+
 import tweepy
 import secret
 
 from bs4 import BeautifulSoup
 import requests
+import json
+import datetime
+
+today = datetime.datetime.today()
 
 # auth = tweepy.OAuthHandler(secret.CONSUMER_KEY, secret.CONSUMER_SECRET)
 # auth.set_access_token(secret.ACCESS_KEY, secret.ACCESS_SECRET)
@@ -11,7 +18,9 @@ import requests
 
 # api.update_status("Another tweet from Becky's laptop")
 
-URL = 'https://stackoverflow.com/questions/14630288/unicodeencodeerror-charmap-codec-cant-encode-character-maps-to-undefined'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup)
+response = requests.get(f'https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key={secret.WEATHER_API_KEY}')
+data = response.json()
+print("***************", today)
+
+# funcs to pull data out of request
+# func to determine time of when to tweet
